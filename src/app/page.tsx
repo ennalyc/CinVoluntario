4e8ea0babf31628a03292682dae3c0488da6b582
ennalyc <ecsl2@cin.ufpe.br>
2025-08-09 10:51:53 -0300

    feat: add page borders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <main className='bg-red-800 w-full h-screen flex justify-center items-center'>
+      <div className='bg-neutral-50 w-16/17 h-13/14 rounded-2xl flex flex-col items-start p-8'>
+        <h1 className='font-bold text-2xl'>Visão Geral</h1>
+      </div>
+    </main>
   )
 }
 
